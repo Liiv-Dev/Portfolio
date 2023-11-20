07f4ac73acc74156ba1c92cb294b20e445a34788
Willie Jones <williejones432@yahoo.com>
2023-11-20 16:02:15 -0600

    Updated page content and animation background

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+function createShootingStar() {
+    const star = document.createElement('div');
+    star.classList.add('shooting-star');
+    document.body.appendChild(star); // Append stars directly to the body
+  
+    // Set initial random positions for each shooting star within the page's width and height
+    const randomX = Math.random() * window.innerWidth;
+    const randomY = Math.random() * window.innerHeight;
+    star.style.left = randomX + 'px';
+    star.style.top = randomY + 'px';
+  
+    setTimeout(() => {
+      star.remove(); // Remove the shooting star after animation ends
+    }, 3000); // 3000ms (3s) is the duration of the shooting star animation
+  }
+  
+  // Function to generate shooting stars every 10 seconds
+  function generateShootingStars() {
+    setInterval(() => {
+      createShootingStar();
+    }, 5000); // Generate a shooting star every 5 seconds (5000ms)
+  }
+  
+  generateShootingStars(); // Start generating shooting stars
+  
+  
